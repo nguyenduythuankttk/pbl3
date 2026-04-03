ee73def8a16576232ba5c7ee7259ca3f224062dd
Wang <nguyenducquang11082006@gmail.com>
2026-04-03 10:49:39 +0700

    test lan dau commit tren github

--- obj/Debug/net10.0/BackEnd.AssemblyInfo.cs
+++ obj/Debug/net10.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9944711d5c528a2c66d16baed1b9fc69b775da35")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
