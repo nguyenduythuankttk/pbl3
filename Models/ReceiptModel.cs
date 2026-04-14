489b5a0ad66974e452884bf0238a918fccac835e
Nguyen Duy Thuan <nguyenduythuan@38tr.local>
2026-04-14 15:18:45 +0700

    Resolve merge conflicts

--- Models/ReceiptModel.cs
+++ Models/ReceiptModel.cs
@@ -26,29 +26,8 @@ namespace Backend.Models{
         [ForeignKey("SupplierID")]
         public virtual Supplier Supplier { get; set; } = null!;
 
-<<<<<<< HEAD
         public DateTime? DateReceive { get; set; } = null;
         public DateTime? DateUpdate {get; set; }
-=======
-        public Guid POID { get; set; }
-
-        [ForeignKey("POID")]
-        public virtual PurchaseOrder PurchaseOrder { get; set; }
-
-        public virtual GoodsInspection? GoodsInspection { get; set; }
-
-        public DateTime DateReceive { get; set; }
-        public ReceiptStatus Status { get; set; }
-        public bool IsDeleted { get; set; } = false;
-        public Guid DeletedBy { get; set; }
-        [ForeignKey("DeletedBy")]
-        public virtual Employee Deleted { get; set; } = null!;
-=======
-        public DateTime DateUpdate {get; set; } = null;
-        public ReceiptStatus Status { get; set; }
-        public bool IsDeleted{get; set;} = false;
-
->>>>>>> 3f996c8133d0c3e2b659425e1aa1cdd644fb15df:Models/ReceiptModel.cs
         [JsonIgnore]
         public virtual ICollection<ReceiptDetail> ReceiptDetails { get; set; } = new List<ReceiptDetail>();
     }
